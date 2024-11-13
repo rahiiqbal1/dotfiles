1c2bdc1f5a61d83d0f9fca43ccd48ddb001ce19d
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-11-13 21:18:48 +0000

    Add bash function to create a nix project flake template

--- bin/functions.sh
+++ bin/functions.sh
@@ -2,3 +2,35 @@ lfcd () {
     # `command` is needed in case `lfcd` is aliased to `lf`
     cd "$(command lf -print-last-dir "$@")"
 }
+
+function mkflk {
+    touch "$1"
+    echo '{
+    description = "";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+        unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { nixpkgs, ... }@inputs:
+        let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in {
+            devShells.${system}.default = pkgs.mkShell {
+                name = "";
+
+                # Specify packages to use.
+                nativeBuildInputs = with pkgs; [
+                ];
+
+                # Set any shell commands to execute on startup.
+                shellHook = '\'''\''
+                '\'''\'';
+
+                # Set project specific environment variables below.
+            };
+        };
+}' > "$1"
+}
