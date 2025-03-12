1aae282127f1e15b70b1970ebd5d007f73a4e49b
Rahi Iqbal <rahi.iqbal1@gmail.com>
2025-03-12 12:38:12 +0000

    Change neovim cursor to catppuccin mocha cols

--- .config/nvim/after/plugin/colours.lua
+++ .config/nvim/after/plugin/colours.lua
@@ -15,9 +15,9 @@ function colourMyPencils()
     vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 
     vim.cmd([[
-        hi Cursor guifg=#A89984 guibg=#A89984 
-        hi Cursor2 guifg=#a9b665 guibg=#a9b665
-        hi Cursor3 guifg=#ea6962 guibg=#ea6962
+        hi Cursor guifg=#89b4fa guibg=#89b4fa 
+        hi Cursor2 guifg=#a6e3a1 guibg=#a6e3a1
+        hi Cursor3 guifg=#cba6f7 guibg=#cba6f7
         set guicursor=n-v-c:block-Cursor/block-Cursor,i-ci-ve:block-Cursor2/block-Cursor2,r-cr:hor20,o:hor50,v-ve:block-Cursor3/block-Cursor3
         ]])
 end
