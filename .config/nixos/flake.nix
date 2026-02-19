7c36f1926232fa28cd940837f14bb8842a915be2
Rahi Iqbal <rahi.iqbal1@gmail.com>
2026-02-19 14:14:01 +0000

    Update nix version to 25.11

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -2,7 +2,7 @@
     description = "Handles package channels and different configs for different hosts";
 
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
         unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
